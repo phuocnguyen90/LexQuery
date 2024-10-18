3f7a6666b59ec9c6894b14aa9471570aa9dc2a66
phuocnguyen90 <phuocnguyen90@yahoo.com>
2024-10-19 00:33:56 +0700

    remove depreciated config file

--- image/src/app/models.py
+++
@@ -1,4 +0,0 @@
-from pydantic import BaseModel
-
-class SubmitQueryRequest(BaseModel):
-    query_text: str
--- image/src/core/logging.py
+++
@@ -1,6 +0,0 @@
-import logging
-
-def setup_logger():
-    logging.basicConfig(level=logging.DEBUG)
-    logger = logging.getLogger("RAGApp")
-    logger.info("Logger is configured")
